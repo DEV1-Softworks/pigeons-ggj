8e7083288dcab780f9a2630e59cae00858b0ffe5
Marco Ramírez <marco.ramirez@rzerocorp.com>
2024-01-28 03:37:18 -0600

    Added Squirrel enemy

---
+++ Assets/Scripts/Enemy/EnemyPatrol.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public GameObject pointA;
+    public GameObject pointB;
+    private Rigidbody2D rb2D;
+
+    //private Animator animator;    
+    private Transform currentPoint;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        // animator = GetComponent<Animator>();
+        currentPoint = pointB.transform;
+        // animator.SetBool("isRunning", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 point = currentPoint.position - transform.position;
+
+        if (currentPoint == pointB.transform)
+        {
+            rb2D.velocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            rb2D.velocity = new Vector2(-speed, 0);
+        }
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
+        {
+            Flip();
+            currentPoint = pointA.transform;
+        }
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
+        {
+            Flip();
+            currentPoint = pointB.transform;
+        }
+    }
+
+    private void Flip()
+    {
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(pointA.transform.position, 0.5f);
+        Gizmos.DrawWireSphere(pointB.transform.position, 0.5f);
+        Gizmos.DrawLine(pointA.transform.position, pointB.transform.position);
+    }
+}
