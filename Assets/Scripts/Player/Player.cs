4bae9386535e6ebc86ff68fccecfe14058e72fa2
Marco Ramírez <marco.ramirez@rzerocorp.com>
2024-01-28 04:50:31 -0600

    Added layer for hazard tiles

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -19,11 +19,17 @@ public class Player : MonoBehaviour
         if (collision.gameObject.CompareTag("Finish"))
         {
             Debug.Log("Win !");
+            Destroy(collision.gameObject);
+            // Send to new scene
         }
         else if (collision.gameObject.CompareTag("Flower"))
         {
             stats.Cottons++;
             Destroy(collision.gameObject);
+        }
+        else if (collision.gameObject.CompareTag("Hazard"))
+        {
+            stats.Health--;
         }
 
     }
