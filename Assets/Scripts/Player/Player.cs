080d94d2cb7ecfa7ec30e145fcbd478af9b2f73d
Marco Ramírez <marco.ramirez@rzerocorp.com>
2024-01-28 04:24:49 -0600

    Finished Flower behaviors, putting them in scenario

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -13,4 +13,18 @@ public class Player : MonoBehaviour
     {
         stats.ResetPlayer();
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Finish"))
+        {
+            Debug.Log("Win !");
+        }
+        else if (collision.gameObject.CompareTag("Flower"))
+        {
+            stats.Cottons++;
+            Destroy(collision.gameObject);
+        }
+
+    }
 }
